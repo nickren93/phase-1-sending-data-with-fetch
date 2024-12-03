af92c5d01510ffc5a600bc8bb047b81a4fc5af3b
Sitong <nickren93@gmail.com>
2024-12-03 02:14:52 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const userInput = {
+        name: userName,
+        email: userEmail,
+    }
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInput),
+    }
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(resp => resp.json())
+    .then(userData => {
+        const id = userData.id
+        document.querySelector("body").append(id)
+    })
+    .catch(error =>{
+        const errorMsg = error.message;
+        document.querySelector("body").append(errorMsg)
+    })
+}
+
